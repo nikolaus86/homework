0026483ca6385bd076790f6eb74fc6e019d56822
w0lf <n_khvorov@mail.ru>
2024-04-10 17:40:37 +0500

    LABA5

---
+++ module1/homework5.py
@@ -0,0 +1,10 @@
+my_list=['Абрикос' ,'Авокадо', 'Австромиртус', 'Азимина', 'Айва', 'Акебия', 'Аки', 'Алыча', 'Ананас', 'Аннона', 'Апельсин', 'Араза','Арбуз', 'Архат', 'Астрокариум']
+print(my_list)
+print(my_list[0],my_list[-1])
+print(my_list[2:5])
+my_list[2]='Воаванга'
+print(my_list)
+my_dict={'Apricot':'Абрикос' ,'Avocado':'Авокадо', 'Austromirtus':'Австромиртус', 'Asimina':'Азимина', 'Quince':'Айва', 'Akebia':'Акебия', 'Aki':'Аки', 'Cherry Plum':'Алыча', 'Pineapple':'Ананас', 'Annona':'Аннона', 'Orange':'Апельсин', 'Araza':'Араза','Watermelon':'Арбуз', 'Arhat':'Архат', 'Astrocarium':'Астрокариум'}
+print(my_dict['Apricot'])
+my_dict['Naranjilla']='Наранхилья'
+print(my_dict)
