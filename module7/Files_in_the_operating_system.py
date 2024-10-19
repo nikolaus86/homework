7f89ca5a809df7274e5692faaf196872473aa409
nikolaus86 <n_khvorov@mail.ru>
2024-10-19 19:56:45 +0500

    update 8.0

---
+++ module7/Files_in_the_operating_system.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+# Указываем путь к директории, по умолчанию это текущая директория
+directory = "."
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)  # Формируем полный путь к файлу
+        filetime = os.path.getmtime(filepath)  # Получаем время последнего изменения файла
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))  # Форматируем время
+        filesize = os.path.getsize(filepath)  # Получаем размер файла
+        parent_dir = os.path.dirname(filepath)  # Получаем родительскую директорию файла
+
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
