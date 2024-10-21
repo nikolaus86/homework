32ed5d2d58b4fae0887483627c52bcd76a26e9ff
nikolaus86 <n_khvorov@mail.ru>
2024-10-22 00:20:05 +0500

    update

--- module18/UrbanDjango/UrbanDjango/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('task2/', include('task2.urls')),
-    path('task3/', include('task3.urls')),
-]
--- module18/UrbanDjango/task2/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def class_view(request):
-    return render(request, 'second_task/class_view.html')
-
-def function_view(request):
-    return render(request, 'second_task/function_view.html')
